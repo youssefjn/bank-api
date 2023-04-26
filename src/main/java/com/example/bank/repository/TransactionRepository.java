d3c3ea352ed22d623eb9f4fadeddf8efe3111690
youssefjn <youssefjneyah@gmail.com>
2023-04-26 09:17:05 +0100

    jpa repos

---
+++ src/main/java/com/example/bank/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.example.bank.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bank.model.Transaction;
+import com.example.bank.model.User;
+
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+    
+    List<Transaction>findByUser(User user);
+
+}
---
+++ src/main/java/com/example/bank/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.bank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bank.model.User;
+
+public interface UserRepository extends JpaRepository <User,Long> {
+    
+}
