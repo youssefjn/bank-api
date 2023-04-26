21a6e536a1e3ba95da1082beaca55ef4be1ef4fe
youssefjn <youssefjneyah@gmail.com>
2023-04-26 09:17:19 +0100

    controllers

---
+++ src/main/java/com/example/bank/controller/TransactionController.java
@@ -0,0 +1,76 @@
+package com.example.bank.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bank.dto.TransactionDTO;
+import com.example.bank.model.Transaction;
+import com.example.bank.model.User;
+import com.example.bank.service.TransactionService;
+import com.example.bank.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/transaction")
+@RequiredArgsConstructor
+public class TransactionController {
+    private final TransactionService transactionService;
+    private final UserService userService;
+
+    @Operation(summary = "Get all transactions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all transactions"),
+            @ApiResponse(responseCode = "204", description = "no transaction found")
+    })
+    @GetMapping
+    public ResponseEntity<?> getAllTransactions() {
+        List<Transaction> transactions = transactionService.findAll();
+        List<TransactionDTO> transactionsDTO = transactions.stream().map(obj -> transactionService.fromDTO(obj))
+                .collect(Collectors.toList());
+        if (transactionsDTO.isEmpty()) {
+            return new ResponseEntity<>(" no Transactions found ", HttpStatus.NO_CONTENT);
+        }
+            return new ResponseEntity<List<TransactionDTO>>(transactionsDTO, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get transaction by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved transaction by id"),
+            @ApiResponse(responseCode = "404", description = "no transaction found")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Transaction transaction = transactionService.findById(id);
+        TransactionDTO transactionDTO = transactionService.fromDTO(transaction);
+        return new ResponseEntity<>(transactionDTO, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get all transactions by user id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all transactions"),
+            @ApiResponse(responseCode = "204", description = "no transactions found"),
+            @ApiResponse(responseCode = "404", description = "no user found")
+    })
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> findByUser(@PathVariable Long id){
+        User user = userService.findById(id);
+        List<Transaction> transactions = transactionService.findByUser(user);
+        List<TransactionDTO> transactionsDTO = transactions.stream().map(obj -> transactionService.fromDTO(obj))
+                .collect(Collectors.toList());
+        if (transactionsDTO.isEmpty()) {
+            return new ResponseEntity<>(" no Transactions found ", HttpStatus.NO_CONTENT);
+        }
+            return new ResponseEntity<List<TransactionDTO>>(transactionsDTO, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/bank/controller/UserController.java
@@ -0,0 +1,91 @@
+package com.example.bank.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bank.dto.UserDTO;
+import com.example.bank.model.Transaction;
+import com.example.bank.model.User;
+import com.example.bank.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all users"),
+            @ApiResponse(responseCode = "204", description = "no user found")
+    })
+    @GetMapping
+    public ResponseEntity<?> getAllusers() {
+        List<User> users = userService.findAll();
+        List<UserDTO> usersDTO = users.stream().map(user -> userService.toDTO(user)).collect(Collectors.toList());
+        if (usersDTO.isEmpty()) {
+            return new ResponseEntity<>(" no Users found ", HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<UserDTO>>(usersDTO, HttpStatus.OK);
+    }
+
+    @Operation(summary = "insert new user")
+    @ApiResponse(responseCode = "201", description = "Successfully inserted")
+    @PostMapping
+    public ResponseEntity<?> insert(@RequestBody UserDTO userDTO) {
+
+        userService.insert(userDTO);
+
+        return new ResponseEntity<UserDTO>( userDTO, HttpStatus.CREATED);    }
+
+    @Operation(summary = "Get user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved user by id"),
+            @ApiResponse(responseCode = "404", description = "no user found")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        UserDTO userDTO = userService.toDTO(user);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @Operation(summary = "make a withdrawal")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "bad request")
+    })
+    @PostMapping("/{id}/withdraw/{value}")
+    public ResponseEntity<Transaction> makeWithdrawal(@PathVariable Long id, @PathVariable Double value) {
+        User user = userService.findById(id);
+        Transaction transaction = userService.makeWithdrawal(user, value);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    @Operation(summary = "make a deposit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "bad request")
+    })
+    @PostMapping("/{id}/deposit/{value}")
+    public ResponseEntity<Transaction> makeDeposit(@PathVariable Long id, @PathVariable Double value) {
+        User user = userService.findById(id);
+        Transaction transaction = userService.makeDeposit(user, value);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+}
